8b4ad869b6775ac2173b526ec2d51070e01cbd8b
Gayan Madushanka <gmadushanka@dpssoftware.co.uk>
2019-02-26 01:16:26 +0530

    GNW.ILS.Service.DocumentTranslator

--- GNW.ILS.WinForms/DocumentTranslatorForm.cs
+++ GNW.ILS.WinForms/DocumentTranslatorForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace GNW.ILS.WinForms
 {
-    public partial class Form1 : Form
+    public partial class DocumentTranslatorForm : Form
     {
-        public Form1()
+        public DocumentTranslatorForm()
         {
             InitializeComponent();
         }
